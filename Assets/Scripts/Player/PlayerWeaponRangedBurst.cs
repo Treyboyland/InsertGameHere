f2b6227bf213b2d04f9b8324344ffbdcf24f738b
Roosevelt Boyland III <treyboyland@aol.com>
2023-03-09 13:49:37 -0600

    Adding variables

--- Assets/Scripts/Player/PlayerWeaponRangedBurst.cs
+++ Assets/Scripts/Player/PlayerWeaponRangedBurst.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerWeaponRangedBurst : MonoBehaviour
+public class PlayerWeaponRangedBurst : PlayerWeapon
 {
+    [SerializeField]
+    ObjectPool pool;
+
+    [SerializeField]
+    OwnerTypeSO playerOwner;
+
+    [SerializeField]
+    GameEventVector weaponFiredEvent;
+
+    [Tooltip("First shot will fire immediately. Last time will act as delay between bursts")]
+    [SerializeField]
+    List<float> shotTimes;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
