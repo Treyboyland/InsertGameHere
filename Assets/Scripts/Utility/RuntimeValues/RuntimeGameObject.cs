ed5c5191c5431ee3f2f69a413044576ba08e4195
Taylor Grote <grotese@gmail.com>
2023-03-16 21:45:08 -0500

    Add RuntimeGameObject

---
+++ Assets/Scripts/Utility/RuntimeValues/RuntimeGameObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RuntimeGameObject", menuName = "RuntimeValues/GameObject")]
+public class RuntimeGameObject : rho.RuntimeValue<GameObject>
+{
+}
