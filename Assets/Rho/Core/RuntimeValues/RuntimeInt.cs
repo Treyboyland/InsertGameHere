c13e0543d5d224239bdef5cbb57459886f137432
Taylor Grote <grotese@gmail.com>
2023-03-18 11:01:40 -0500

    Add Modify method to RuntimeInt

--- Assets/Rho/Core/RuntimeValues/RuntimeInt.cs
+++ Assets/Rho/Core/RuntimeValues/RuntimeInt.cs
@@ -5,5 +5,6 @@ namespace rho
     [CreateAssetMenu(fileName = "RuntimeIntValue", menuName = "Rho/RuntimeValues/Int")]
     public class RuntimeInt : RuntimeValue<int>
     {
+        public void Modify(int magnitude) => Value += magnitude;
     }
 }
