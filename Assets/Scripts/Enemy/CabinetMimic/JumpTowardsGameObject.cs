1b2966ae176c1728b139bf334a5f3cf84d73a6cb
Taylor Grote <grotese@gmail.com>
2023-03-25 10:27:43 -0500

    Add Jump to cabinet mimic

---
+++ Assets/Scripts/Enemy/CabinetMimic/JumpTowardsGameObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class JumpTowardsGameObject : MonoBehaviour
+{
+    [SerializeField, NaughtyAttributes.Required] RuntimeGameObject _target;
+    [SerializeField, NaughtyAttributes.Required] Rigidbody2D _body;
+    [SerializeField] Animator _animator;
+    [SerializeField, NaughtyAttributes.AnimatorParam("_animator", AnimatorControllerParameterType.Trigger)] string _jumpTrigger;
+    [SerializeField] float _speed;
+    [SerializeField] float _interJumpDelay;
+
+    bool _isJumping = false;
+
+    public void ResetJumpFlag() => _isJumping = false;
+
+    IEnumerator JumpTowardsTarget()
+    {
+        while (true)
+        {
+            if (_target.Value == null)
+            {
+                yield return null;
+            }
+            // Find Player
+            var targetPosition = _target.Value.transform.position;
+            // Start Jump Animation
+            _animator.SetTrigger(_jumpTrigger);
+            _isJumping = true;
+
+            while (_isJumping)
+            {
+                yield return new WaitForFixedUpdate();
+                var newPosition = Vector2.MoveTowards(_body.position, targetPosition, _speed * Time.fixedDeltaTime);
+                _body.MovePosition(newPosition);
+            }
+            yield return new WaitForSeconds(_interJumpDelay);
+        }
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(JumpTowardsTarget());
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();        
+    }
+}
