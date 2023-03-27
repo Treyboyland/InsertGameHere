052582dbc292a6783090125316153835004393c8
Taylor Grote <grotese@gmail.com>
2023-03-27 16:19:28 -0500

    Add Eating Player Animation to trigger on trigger collision

--- Assets/Scripts/Enemy/CabinetMimic/CabinetMimicAnimationEventListener.cs
+++ Assets/Scripts/Enemy/CabinetMimic/CabinetMimicAnimationEventListener.cs
@@ -11,4 +11,9 @@ public class CabinetMimicAnimationEventListener : MonoBehaviour
     public void OnTransformationComplete() => _onTransformationComplete.Invoke();
 
     public void OnJumpComplete() => _onJumpComplete.Invoke();
+
+    [SerializeField] Animator _animator;
+    [SerializeField, NaughtyAttributes.AnimatorParam("_animator", AnimatorControllerParameterType.Bool)] 
+    string _eatingParam;
+    public void SetEatingPlayerFlag(bool value) => _animator.SetBool(_eatingParam, value);
 }
