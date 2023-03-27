7369a8d5f67b36b22acced240041be8b45e4ab1d
Taylor Grote <grotese@gmail.com>
2023-03-27 13:34:38 -0500

    Combine CabinetMimic Specific animation event listeners into one script

---
+++ Assets/Scripts/Enemy/CabinetMimic/CabinetMimicAnimationEventListener.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CabinetMimicAnimationEventListener : MonoBehaviour
+{
+    [SerializeField] UnityEvent _onTransformationComplete;
+    [SerializeField] UnityEvent _onJumpComplete;
+
+    public void OnTransformationComplete() => _onTransformationComplete.Invoke();
+
+    public void OnJumpComplete() => _onJumpComplete.Invoke();
+}
