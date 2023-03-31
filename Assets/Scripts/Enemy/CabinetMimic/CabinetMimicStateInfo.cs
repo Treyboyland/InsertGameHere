e84894b204c92427f245cdc8021ef01ffcab713d
Taylor Grote <grotese@gmail.com>
2023-03-31 11:16:38 -0500

    Fix Mimic not flipping left after transformation

--- Assets/Scripts/Enemy/CabinetMimic/CabinetMimicStateInfo.cs
+++ Assets/Scripts/Enemy/CabinetMimic/CabinetMimicStateInfo.cs
@@ -8,7 +8,7 @@ public class CabinetMimicStateInfo : MonoBehaviour
     public GameObject ChewingTarget { get; set; }
     public void SetChewingTarget(RuntimeGameObject rtgo) => ChewingTarget = rtgo.Value;
 
-    bool _lookingLeft = true;
+    bool _lookingLeft = false;
     public bool LookingLeft 
     {
         get => _lookingLeft;
