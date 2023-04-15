2c6fb5a4941eb43580565e04b81889c4b37b6c90
Taylor Grote <grotese@gmail.com>
2023-04-15 12:33:46 -0500

    Add Tint override to Flash Material and Handler

--- Assets/Scripts/Effects/FlashEffectHandler.cs
+++ Assets/Scripts/Effects/FlashEffectHandler.cs
@@ -3,19 +3,24 @@ using UnityEngine;
 public class FlashEffectHandler : MonoBehaviour
 {
     [SerializeField, NaughtyAttributes.Required] SpriteRenderer _spriteRenderer;
+    Color _tint = Color.white;
     [SerializeField, Range(0, 1)] float _flashAmount;
     [SerializeField] Color _flashColor = Color.white;
     [SerializeField] string _flashAmountParam = "_FlashAmount";
     [SerializeField] string _flashColorParam = "_FlashColor";
-
-    void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
+    [SerializeField] string _tintParam = "_Tint";
 
     void Update()
     {
+        // If the sprite renderer is never not white, then steal that color and use it as tint
+        if (_spriteRenderer.color != Color.white)
+        {
+            _tint = _spriteRenderer.color;
+            _spriteRenderer.color = Color.white;
+        }
+        
         _spriteRenderer.material.SetColor(_flashColorParam, _flashColor);
         _spriteRenderer.material.SetFloat(_flashAmountParam, _flashAmount);
+        _spriteRenderer.material.SetColor(_tintParam, _tint);
     }
 }
