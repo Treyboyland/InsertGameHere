8fbfb5ef9939d0ce4e281cd9ff135ef85f30ceff
Taylor Grote <grotese@gmail.com>
2023-04-15 12:03:20 -0500

    Update Flash Effect to take Sprite Renderer in as inspector injection

--- Assets/Scripts/Effects/FlashEffectHandler.cs
+++ Assets/Scripts/Effects/FlashEffectHandler.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class FlashEffectHandler : MonoBehaviour
 {
+    [SerializeField, NaughtyAttributes.Required] SpriteRenderer _spriteRenderer;
     [SerializeField, Range(0, 1)] float _flashAmount;
     [SerializeField] Color _flashColor = Color.white;
     [SerializeField] string _flashAmountParam = "_FlashAmount";
     [SerializeField] string _flashColorParam = "_FlashColor";
-
-    SpriteRenderer _spriteRenderer;
 
     void Awake()
     {
