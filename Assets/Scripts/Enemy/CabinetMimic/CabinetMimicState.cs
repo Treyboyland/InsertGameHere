b8f5c7d8a40b4870f1c93a6092c08fa277edc4ca
Taylor Grote <grotese@gmail.com>
2023-04-21 10:58:37 -0500

    Remove unused cabinet mimic script assets

--- Assets/Scripts/Enemy/CabinetMimic/CabinetMimicState.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "New Cabinet Mimc State")]
-public class CabinetMimicState : ScriptableObject
-{
-}
--- Assets/Scripts/Enemy/CabinetMimic/CabinetMimicStateMachine.cs
+++
@@ -1,3 +0,0 @@
-public class CabinetMimicStateMachine : SOStateHandler<CabinetMimicState>
-{
-}
